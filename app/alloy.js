8e7bf60892c0e772dfa7aebf325619dd13d6470d
simarpreetsinghkalra <simarpreetsinghkalra@gmail.com>
2020-06-01 17:01:07 +0530

    Updated Airship 6.0.0 Android

--- app/alloy.js
+++ app/alloy.js
@@ -9,3 +9,42 @@
 // object. For example:
 //
 // Alloy.Globals.someGlobalFunction = function(){};
+var UrbanAirship = require('ti.airship');
+Alloy.Globals.urbanairship = UrbanAirship;
+
+
+var user_info = JSON.parse(Ti.App.Properties.getString('user_info'));
+UrbanAirship.namedUser = user_info ? user_info.email : '';
+UrbanAirship.tags = [ 'yofima' ];
+UrbanAirship.userNotificationsEnabled = true;
+UrbanAirship.setUserNotificationsEnabled(true);
+Ti.API.info("Launch: " + UrbanAirship.getLaunchNotification(true).message);
+Ti.API.info("Launch Deep Link: " + UrbanAirship.getDeepLink(true));
+
+if (OS_IOS) {
+    Ti.App.addEventListener('resumed', function() {
+        Ti.API.info("Launch iOS resumed: " + UrbanAirship.getLaunchNotification(true).message);
+    });
+}
+
+UrbanAirship.addEventListener(UrbanAirship.EVENT_DEEP_LINK_RECEIVED, function (e) {
+    Alloy.Globals.alert("Received deepLink: " + e.deepLink);
+});
+
+UrbanAirship.addEventListener(UrbanAirship.EVENT_CHANNEL_UPDATED, function(e) {
+    Ti.API.info('Channel Updated: ' + UrbanAirship.channelId);
+    Alloy.Globals.push_token = UrbanAirship.channelId;
+    if (user_info) {
+    	Alloy.Globals.syncAction('setdevice', UrbanAirship.channelId);
+    }
+});
+
+UrbanAirship.addEventListener(UrbanAirship.EVENT_PUSH_RECEIVED, function(e) {
+    Ti.API.info('Push received: ' + e.message);
+    Ti.API.info('Extras: ' + JSON.stringify(e.extras));
+    if (!Alloy.Globals.messagingwindowopen) {
+        Alloy.Globals.alert(e.message);
+    } else {
+  	    Ti.App.fireEvent('messaging_push_received');
+    }
+});
